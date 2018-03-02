0f0b2643059c2a244ead31eea48d623370203d88
Jackson Machado <jackson.machado@ng.local>
2018-03-02 17:20:44 -0300

    Aula 268

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,7 +1,22 @@
 import { Usuario } from "./acesso/usuario.model";
+import * as firebase from 'firebase';
 
 export class ServicoAutenticacao {
 	public cadastrarUsuario(usuario: Usuario): void {
-		console.log(usuario)
+		firebase.auth().createUserWithEmailAndPassword(
+			usuario.email, usuario.senha
+		)
+		.then((resposta: any) => {
+
+			//remove atributo de senha
+			delete usuario.senha
+			console.log(usuario)
+			//registro dos dados complementares do usuário no path email da base64
+			firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+				.set( usuario )
+
+			console.log(usuario)
+		})
+		.catch((error: Error) => console.log( error ))
 	}
 }
