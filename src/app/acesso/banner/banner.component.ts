25e990c17ed9884249adb844a59bd2bae2562f8d
Jackson Machado <jackson.machado@ng.local>
2018-03-02 13:31:55 -0300

    Aula 249

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
+import { Imagem } from './imagem.model'
 
 @Component({
   selector: 'app-banner',
@@ -19,6 +20,13 @@ import { Component, OnInit, trigger, state, style, transition, animate } from '@
 export class BannerComponent implements OnInit {
 
   public estado: string = 'escondido'
+  public imagens: Array<Imagem> = [
+	  { estado: 'visivel', url: '/assets/banner-acesso/img_1.png'},
+	  { estado: 'escondido', url: '/assets/banner-acesso/img_2.png'},
+	  { estado: 'escondido', url: '/assets/banner-acesso/img_3.png'},
+	  { estado: 'escondido', url: '/assets/banner-acesso/img_4.png'},
+	  { estado: 'escondido', url: '/assets/banner-acesso/img_5.png'}
+  ]
 
   constructor() { }
 
