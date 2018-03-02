1176ba91d4048bcb79ec40abbb07b91a8feef32e
Jackson Machado <jackson.machado@ng.local>
2018-03-02 15:26:36 -0300

    Aula 261

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Usuario } from '../usuario.model';
 
 @Component({
   selector: 'app-cadastro',
@@ -23,6 +24,11 @@ export class CadastroComponent implements OnInit {
 	this.exibirPainel.emit('login')
   }
   public cadastrarUsuario(): void{
-	  console.log(this.formulario)
+	  let usuario: Usuario = new Usuario(
+		  this.formulario.value.email,
+		  this.formulario.value.nome_completo,
+		  this.formulario.value.nome_usuario,
+		  this.formulario.value.senha)
+	  console.log(usuario)
   }
 }
