99f32ad1470a0236740f02c09ca5829834efdcee
Jackson Machado <jackson.machado@ng.local>
2018-03-05 15:39:41 -0300

    Aula 281

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -3,10 +3,14 @@ import { Router } from '@angular/router';
 import { Usuario } from "./acesso/usuario.model";
 import * as firebase from 'firebase';
 
+import { Observable } from 'rxjs/Observable';
+
 @Injectable()
 export class ServicoAutenticacao {
 
 	public token_id: string
+	public error_msg: string
+
 	constructor(private router: Router) {
 
 	}
@@ -23,19 +27,24 @@ export class ServicoAutenticacao {
 						.set( usuario )
 
 				})
-				.catch((error: Error) => console.log( error ))
+				.catch((error: Error) => {
+					this.error_msg = error.message
+				})
 	}
-	public autenticar(email: string, senha: string): void {
-		firebase.auth().signInWithEmailAndPassword(email, senha)
-			.then((resposta: any) => {
-				firebase.auth().currentUser.getIdToken()
-					.then( (idToken: string) => {
-						this.token_id = idToken
-						localStorage.setItem('idToken', idToken)
-						this.router.navigate(['/home'])
+	public autenticar(email: string, senha: string): Promise<any> {
+		return firebase.auth().signInWithEmailAndPassword(email, senha)
+					.then((resposta: any) => {
+						firebase.auth().currentUser.getIdToken()
+							.then( (idToken: string) => {
+								this.token_id = idToken
+								localStorage.setItem('idToken', idToken)
+								this.router.navigate(['/home'])
+							})
+						return ''
 					})
-			})
-			.catch((erro: Error) => console.log(erro))
+					.catch((error: Error) => {
+						return error.message
+					})
 	}
 	public autenticado(): boolean {
 
