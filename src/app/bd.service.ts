edd2d6dc2bfd841f47f323a652ee81048b0a2edc
Jackson Machado <jackson.machado@ng.local>
2018-03-05 17:29:40 -0300

    Aula 293

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -8,23 +8,26 @@ export class Bd {
 	constructor(private progresso: Progresso) {}
 	public publicar(publicacao: any): void {
 
-		let nomeImagem = Date.now()
 
-		firebase.storage().ref()
-			.child(`imagens/${nomeImagem}`)
-			.put(publicacao.imagem)
-			.on(firebase.storage.TaskEvent.STATE_CHANGED,
-					(snapshot: any) => {
-						this.progresso.status = 'andamento'
-						this.progresso.estado = snapshot
-					},
-					(error) => {
-						this.progresso.status = 'erro'
-					},
-					() => {
-						this.progresso.status = 'concluido'
-					})
-		/*firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
-			.push( { titulo: publicacao.titulo })*/
+		firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+			.push( { titulo: publicacao.titulo })
+			.then( (resposta: any) =>{
+				let nomeImagem = resposta.key
+				firebase.storage().ref()
+				.child(`imagens/${nomeImagem}`)
+				.put(publicacao.imagem)
+				.on(firebase.storage.TaskEvent.STATE_CHANGED,
+						(snapshot: any) => {
+							this.progresso.status = 'andamento'
+							this.progresso.estado = snapshot
+						},
+						(error) => {
+							this.progresso.status = 'erro'
+						},
+						() => {
+							this.progresso.status = 'concluido'
+						})
+			})
+
 	}
 }
