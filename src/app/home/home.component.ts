1baac0f17eb3ff986a78d6f57712fbf0d578b28f
Jackson Machado <jackson.machado@ng.local>
2018-03-05 18:18:55 -0300

    Aula 300

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ServicoAutenticacao } from '../autenticacao.service';
 
 @Component({
@@ -8,6 +8,8 @@ import { ServicoAutenticacao } from '../autenticacao.service';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('publicacoes') public publicacoes: any
+
   constructor(private servicoAutenticacao: ServicoAutenticacao) { }
 
   ngOnInit() {
@@ -16,6 +18,6 @@ export class HomeComponent implements OnInit {
 	this.servicoAutenticacao.sair()
   }
   public atualizarTimeLine(): void {
-
+	this.publicacoes.atualizarTimeLine()
   }
 }
